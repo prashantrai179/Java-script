0733b3a9efa3f21de5b89411bdec53de818dc2fe
prashantrai179 <prashantrai179@gmail.com>
2025-09-13 20:10:04 +0530

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,5 +21,46 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber) 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ************************* operations *********************
+
+let value = 5
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Preeti"
+
+let str3 = str1 + str2 
+//console.log(str3);
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
+// console.log((1 + 2) * 3 % 6);
+
+//tricky conversion  
+// console.log(+true);
+// console.log(+"");
+
+// this is not a right conversion
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
