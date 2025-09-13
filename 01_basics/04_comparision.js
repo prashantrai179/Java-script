99f2a35ef796678f9018b8e813919f62ee3b49f6
prashantrai179 <prashantrai179@gmail.com>
2025-09-13 22:37:37 +0530

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,22 @@
+// less than, greater than , etc => this all come with boolean value
+
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("9" > 5);
+// console.log("02" > 5);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);                                             
+
+// ===
+
+console.log("2" === 1);
