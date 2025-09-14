e3d2f56dd3416ca693f35a929c60c724ab66c287
prashantrai179 <prashantrai179@gmail.com>
2025-09-14 20:11:56 +0530

    numbers and math

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,34 @@
+const score = 200
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNUmber = 432.8323
+// console.log(otherNUmber.toPrecision(3));
+
+const zeros = 10000000
+// console.log(zeros.toLocaleString('en-IN'));
+
+// ----------------------------- Math -----------------------------------------------
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(8.4));
+// console.log(Math.ceil(9.1));
+// console.log(Math.floor(9.1));
+// console.log(Math.sqrt(25));
+// console.log(Math.min(2,4,5,3,1));
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
