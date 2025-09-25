922885805e7deb7fe892fdf77ec4a07013337b9a
prashantrai179 <prashantrai179@gmail.com>
2025-09-26 00:22:03 +0530

    Scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,22 @@
+// let a = 10
+// const b = 20
+// var c = 30
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+//var c = 300       // global scope
+
+if(true){
+    let a = 10
+    const b = 20
+}                  // block scope
+
+// console.log(a);
+// console.log(b);
+console.log(c);
+
+
+
