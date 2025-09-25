283125f26778a6597c6701e7942f22f91f9b53fa
prashantrai179 <prashantrai179@gmail.com>
2025-09-25 18:47:43 +0530

    Function in deep

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -40,7 +40,7 @@ const result = addTwoNum(4,5)
 
 function userLoginMessage(username){  // (username = "rai") so if condition is not working
     if(username === undefined){  //(!username)
-        console.log("Please enter your username");
+        // console.log("Please enter your username");
         return
     }
     return `${username} you just logged in`
@@ -49,4 +49,39 @@ function userLoginMessage(username){  // (username = "rai") so if condition is n
 
 // console.log(userLoginMessage("preet"))
 
-console.log(userLoginMessage())
+// console.log(userLoginMessage())
+
+// function calculateCardprice(...num){
+//     return num
+// }
+// console.log(calculateCardprice(200,543,7897));
+
+function calculateCardprice(val1, val2,...num){
+    return num
+}
+// console.log(calculateCardprice(200,543,789,342,123));
+
+const user = {
+    username : "preet",
+    price : 30002
+}
+
+function handleObject(kucbhi){
+    console.log(`username is ${kucbhi.username} and price is ${kucbhi.price}`)
+}
+
+// handleObject(user)
+handleObject({
+    username : "sam",
+    price : 40002
+})
+
+
+const myNewArray = [300, 400, 600, 100]
+
+function returnSecondValue(getArray) {
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([300, 400, 600, 100]));
