ea8500d246126a783f00220a053059ed842f743b
prashantrai179 <prashantrai179@gmail.com>
2025-09-27 18:44:19 +0530

    Scope and Nested

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -23,10 +23,40 @@ let a = 300
 if(true){
     let a = 10
     const b = 20
-    console.log("INNER", a);
+    // console.log("INNER", a);
 }  
 
-console.log(a);
+// console.log(a);
 
 
+// nested 
+
+function one(){
+    const username = "Preet"
+
+    function two(){
+        const teacher = "youtube"
+        console.log(username);
+    }
+    // console.log(teacher);  //out of scope
+    
+    two()
+}
+// one()
+
+
+//--------------------------- Intresting -----------------------------
+
+// console.log(addone(5));
+
+// function addone(num){
+//     return num + 1
+// }
+
+
+addtwo(5)
+
+const addtwo = function(num){
+    return num + 2
+}
 
