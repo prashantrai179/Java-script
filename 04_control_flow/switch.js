16606e58cd402750c483eeb8a2fac2e47c4c5fbc
prashantrai179 <prashantrai179@gmail.com>
2025-09-30 17:15:21 +0530

    Switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,35 @@
+// syntax
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 5
+
+switch (month) {  // key = The value I need to check every time
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+
+    default:
+        console.log("Default case match");
+        
+        break;
+}
