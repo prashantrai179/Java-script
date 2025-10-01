fb63aa72fd2c93717f7bf56833f6bcbb8e01ece4
prashantrai179 <prashantrai179@gmail.com>
2025-10-01 16:58:19 +0530

    Again

---
+++ 05_iterations/three_More_loops.js
@@ -0,0 +1,42 @@
+// for of 
+// for-of loop is used for arrays
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1,2,3,4,5,6,6]
+
+// for (const num of arr) {
+//     console.log(num);  
+// }
+
+const greeting = "Hello World"
+for (const greet of greeting) {
+    // console.log(`Each char is ${greet}`);
+}
+
+// Maps  -> it store unique value
+
+const map = new Map() // map apne aap mai obj hai and in js all are objects.
+map.set('IN', "India")
+map.set('fr', "France")
+map.set('US', "United State")
+map.set('IN', "India")
+
+// console.log(map);
+
+
+for (const [key, value] of map) {
+    console.log(key, ':-', value); 
+}
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// for (const [key,value] of myObject) {
+//     console.log(key, ':-', value); 
+// }  
+// Not working this forof on this above object
+
